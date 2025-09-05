5f92e296fa9769e9bb89bbbf0c9fd2eb2f2b440c
TuNuevoUsuario <maysilvaespinoza@crackthecode.la>
2025-09-05 11:28:53 -0500

    Unidad 34 Agregue de la clase sobre el tema de array

--- S34/assets/js/scripth.js
+++ S34/assets/js/scripth.js
@@ -78,7 +78,7 @@ console.log(fruits.slice(2, 4));
 //Reto 6
 
 console.log("splice");
-// splice
+// splice(inicio, cantidad, nuevos elementos...) -> Modifica el arreglo ORIGINAL. Desde "inicio" se eliminan la "cantidad" de elementos indicado.
 
 //Corta hasta el indice 2 exclusivo 
 //fruits.splice(2)
@@ -88,7 +88,56 @@ console.log("splice");
 //fruits.splice(2, 2);
 //console.log(fruits);
 
-//
+//Desde el indice 2, elimina dos elementos y desde esa misma posicion agrega los nuevos elementos.
 fruits.splice(2,2, "sandia", "kiwi", "arándano");
 console.log(fruits);
+
+//Reto7
+
+console.log("================================");
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(nums);
+
+console.log("map");
+//map(funcion) -> Retorna una COPIA del array. Aplica la funcion a cada uno de los elementos del array. El array resultante tiene la misma cantidad del elementos. 
+
+//A cada elemento le aumenta 3
+let numsMap = nums.map((x) => x + 3);
+console.log(numsMap);
+
+//Multiplica los elementos por 2
+console.log(nums.map((x) => x * 10));
+
+//Mostrar strings
+console.log(nums.map((x) => `Numero ${x}`));
+
+//Reto 8
+
+console.log("filter");
+//filter(funcion) -> Retorna una COPIA des array original. Aplica una funcion de flitrado a todos los elementos del array y solo devuelve los que cumplen la condicion.
+
+//Filtra los elementos mayores que 5
+let filterNums = nums.filter((x) => x > 5);
+console.log(filterNums);
+
+//Filtra los elementos menores que 6
+console.log(nums.filter((x) => x < 6));
+
+//Filtra los elementos pares
+console.log(nums.filter((x) => x % 2 ===0));
+
+//Reto 9
+
+console.log("reduce")
+//reduce(funcion, acumulador) -> Retorna un valor que es el resultado de reducir el array. Lo reduce aplicando la funcion a los elementos, ademas va guardando el resultado en el acumulador,
+
+//Suma todos los elementos del array 
+console.log(nums.reduce((a, b) => a + b, 0));
+
+//Multiplica todos los elementos del array
+console.log(nums.reduce((a, b) => a * b, 1));
+
+console.log("=================================")
+
+//Reto 10
 
